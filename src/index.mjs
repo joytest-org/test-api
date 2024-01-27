ace28b65980cf43fb9467ceaacdda1d2c09baa83
dasdeo <mark@anio.software>
2024-01-27 13:38:58 +0100

    Make label optional.

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,6 @@
 import createRandomIdentifier from "@anio-js-core-foundation/create-random-identifier"
 
-export function createTestSuite(referenced_from, label) {
+export function createTestSuite(referenced_from, label = null) {
 	let context = {
 		suite: {
 			// Will be the same if test suite is imported again
