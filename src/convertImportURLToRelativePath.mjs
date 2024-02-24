b0f765c611f80770638768d95b38b6a7da735030
dasdeo <mark@anio.software>
2024-02-24 10:40:17 +0100

    Look for nearest package.json instead of anio_project.mjs to make this project more versatile and to not force users into creating an additional (and pointless) configuration file.

--- src/convertImportURLToRelativePath.mjs
+++ src/convertImportURLToRelativePath.mjs
@@ -2,7 +2,7 @@
  * This function shall convert the absolute import.meta.url
  * passed to createTestSuite into a relative path.
  * In a node context, it will use @anio-js-core-foundation/node-find-nearest-file
- * to find anio_project.mjs and assume the project root from there.
+ * to find package.json and assume the project root from there.
  *
  * In a browser, it is assumed that all project files are located at [url]/project_files/
  */
@@ -26,11 +26,19 @@ function convertImportURLToRelativePathNode(import_url) {
 
 	const absolute_import_url = fs.realpathSync(fileURLToPath(import_url))
 	const project_config_path = findNearestFile(
-		"anio_project.mjs", path.dirname(absolute_import_url)
+		"package.json", path.dirname(absolute_import_url)
 	)
 
 	const project_root = path.dirname(project_config_path)
 	const relative_path = path.relative(project_root, absolute_import_url)
+
+	//
+	// todo:
+	//
+	// as an additional safe-guard, compare
+	// a globally set project root path (by anio-jtest cli)
+	// with the value "project_root"
+	//
 
 	return normalizePath(`/${relative_path}`)
 }
